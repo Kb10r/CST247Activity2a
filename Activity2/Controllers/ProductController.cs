c1d6bf2335738e7ccbf9902750859a4a6e73fac8
Kb10r <54547103+Kb10r@users.noreply.github.com>
2021-01-31 23:43:52 -0700

    Initial Upload Activity2a

---
+++ Activity2/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult message()
+        {
+            return View("message");
+        }
+
+        public IActionResult welcome(String name, int secretNumber=13) 
+        {
+            ViewBag.Name = name;
+            ViewBag.Secret = secretNumber;
+
+            return View("welcome");
+        }
+    }
+}
